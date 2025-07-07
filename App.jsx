7903c8ae37456c829e1bb00a7572a06bd939c1a9
vidisha108 <vidishasrivastava2001@gmail.com>
2025-07-07 20:02:17 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+
+import MainSection from './components/MainSection/MainSection'
+import {Sidebar} from './components/Sidebar/Sidebar'
+function App() {
+  
+
+  return (
+    <>
+      <Sidebar />
+      <MainSection/>
+    </>
+  )
+}
+
+export default App
